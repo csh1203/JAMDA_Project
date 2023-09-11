c33a9d94b1f007bc14bdfd457e0308fb0bcabc39
jangjian <pureunnabi@gmail.com>
2023-09-12 03:36:43 +0900

    비밀번호 변경, 프로필 수정

--- app/src/main/assets/js/editPw.js
+++ app/src/main/assets/js/editPw.js
@@ -1,20 +1,45 @@
-function change(){
-    let pw1 = document.getElementsByClassName('input-pw')[1].value;
-    let pw_check = document.getElementById('pw-check').value;
+function change() {
+    const currentPw = document.getElementById("current-pw").value;
+    const newPw = document.getElementById("new-pw").value;
+    const confirmPw = document.getElementById("confirm-pw").value;
+
+    const token = localStorage.getItem("token");
+  
+    // 현재 비밀번호, 새 비밀번호, 비밀번호 확인의 유효성을 검사합니다.
+    if (!currentPw || !newPw || !confirmPw) {
+      alert("비밀번호를 입력하세요.");
+      return;
+    }
+  
+    if (newPw !== confirmPw) {
+      alert("새 비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+      return;
+    }
 
     function validatePassword(password) {
         const passwordRegex = /^(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[a-z\d@$!%*?&]{6,20}$/i;
         return passwordRegex.test(password);
     }
 
-    if (!validatePassword(pw1)) {
+    if (!validatePassword(newPw)) {
         alert("비밀번호는 영문 소문자, 숫자, 특수문자를 사용하여 6~20자로 이루어져야 합니다.");
         return;
-      }
-
-    if(pw1 !== pw_check){
-        alert('새로운 비밀번호를 일치하지 않습니다.');
-        return 0;
     }
-    window.location.href='./editIdPw.html';
-}
+  
+    // 서버로 비밀번호 변경 요청을 보냅니다.
+    axios
+      .post("http://43.201.10.121:3000/users/change-password", {
+        currentPassword: currentPw,
+        newPassword: newPw,
+      }, {headers: { authorization: token }})
+      .then((response) => {
+        alert(response.data.message); // 비밀번호 변경 결과 메시지를 표시
+        // 비밀번호 변경이 성공하면 필요한 작업을 수행하세요.
+        window.location.href='./editIdPw.html';
+      })
+      .catch((error) => {
+        console.error("비밀번호 변경 중 오류 발생:", error);
+        alert("비밀번호 변경 중 오류가 발생했습니다.");
+      });
+  }
+  
--- app/src/main/assets/js/profileSetting.js
+++ app/src/main/assets/js/profileSetting.js
@@ -1,32 +1,60 @@
 onchange = e => {
-    let reader = new FileReader();
-    let name = e.target.files[0].name
-    let date = e.target.files[0].lastModified
-    reader.onloadend = () => {
-      // 2. 읽기가 완료되면 아래코드가 실행됩니다.
-      const base64 = reader.result;
-      if (base64) {
-        // 3. 리스트에 저장합니다.
-        async function imageHandle(e){
-          images = [...images, {
-          image : base64.toString(),
-          name,
-          date
-        }
-        ];
+  let reader = new FileReader();
+  let name = e.target.files[0].name
+  let date = e.target.files[0].lastModified
+  reader.onloadend = () => {
+    // 2. 읽기가 완료되면 아래코드가 실행됩니다.
+    const base64 = reader.result;
+    if (base64) {
+      // 3. 리스트에 저장합니다.
+      async function imageHandle(e){
+        images = [...images, {
+        image : base64.toString(),
+        name,
+        date
       }
+      ];
     }
-    
-    if (e.target.files[0]) {
-      reader.readAsDataURL(e.target.files[0]); // 1. 파일을 읽어 버퍼에 저장합니다.
-    }
-    document.getElementById("imageCamera").value = null
   }
+  
+  if (e.target.files[0]) {
+    reader.readAsDataURL(e.target.files[0]); // 1. 파일을 읽어 버퍼에 저장합니다.
+  }
+  document.getElementById("imageCamera").value = null
+}
 }
 
 function check(){
-  window.location.href="./setting.html";
+
 }
+
+function check() {
+const name = document.getElementById('name').value;
+const bias = document.getElementById('bias').value;
+const weight = document.getElementById('weight').value;
+const goal_weight = document.getElementById('goal_weight').value;
+
+const token = localStorage.getItem("token");
+
+// Axios를 사용하여 서버로 요청 보내기
+axios
+.post("http://43.201.10.121:3000/users/update-profile", {
+  name: name,
+  bias: bias,
+  weight: weight,
+  goal_weight: goal_weight,
+}, {headers: { authorization: token }},)
+  .then((response) => {
+    console.log('프로필이 업데이트되었습니다.', response.data);
+    window.location.href="./setting.html";
+  })
+  .catch((error) => {
+    // 업데이트 중 오류 발생 시 처리
+    console.error('프로필 업데이트 중 오류 발생', error);
+    // 에러 처리 (예: 오류 메시지 표시)
+  });
+}
+
 
 // onChange={e => {
 //     let reader = new FileReader();
