a759b1d2ea8b140af36167be40392bfcd6141d38
Jian Jang <pureunnabi@gmail.com>
2023-09-14 00:28:24 +0900

    아이디, 비밀번호 찾기 인증코드 전송

--- app/src/main/assets/js/findIdPw.js
+++ app/src/main/assets/js/findIdPw.js
@@ -34,8 +34,6 @@ function disabledBtn(object){
             btn[0].nextElementSibling.style.background = "rgba(255, 102, 102, 1)";
             btn[0].disabled = true;
             btn[1].disabled = false;
-            console.log(btn[0]);
-            console.log(btn[1]);
             find[0].style.background ="rgba(255, 194, 194, 1)";
             find[0].disabled = true;
         }
@@ -47,14 +45,10 @@ function disabledBtn(object){
             btn[0].disabled = false;
             btn[0].disabled = false;
             find[0].disabled = false;
-            console.log(btn[0]);
-            console.log(btn[1]);
-            console.log(find[0]);
             notEdit();
         }
         else{}
     }
-    certificationNumber(object);
 }
 function notEdit(){
     find[0].style.background= "rgba(255, 102, 102, 1)";
@@ -62,3 +56,56 @@ function notEdit(){
     btn[1].nextElementSibling.disabled=true;
     btn[1].nextElementSibling.style.background = "rgba(255, 194, 194, 1)";
 }
+
+// 인증번호 보내는 함수
+function emailsubmit() {
+    var email = document.getElementById("email").value;
+    var authCode = document.getElementById("auth").value;
+  
+    // 아이디 중복 확인 API 엔드포인트 수정
+    axios
+    .post("http://localhost:3000/users/certificate", {
+      email: email,
+      authCode : authCode
+    })
+    .then((response) => {
+      alert("이메일을 발송했습니다.");
+    })
+    .catch((e) => {
+    console.error("Error during duplicate check:", e);
+    alert("에러가 발생했습니다.");
+    });
+  }  
+    
+    
+  let isEmailVerified = false; // 이메일 인증 상태를 나타내는 변수
+    
+  // 인증번호 확인 함수
+  function checkAuthCode() {
+    var email = document.getElementById("email").value;
+    var authCode = document.getElementById("auth").value; // 입력한 인증번호 가져오기
+  
+    if (authCode.trim() === "") {
+      alert("인증번호를 입력해주세요.");
+      return;
+    }
+  
+    axios
+      .post("http://localhost:3000/users/check-auth-code", {
+        email: email,
+        code: authCode,
+      })
+      .then((response) => {
+        if (response.status === 200 && response.data.message === '인증번호가 확인되었습니다.') {
+          isEmailVerified = true; // 이메일 인증 성공
+          alert("인증번호가 확인되었습니다.");
+        } else {
+          isEmailVerified = false; // 이메일 인증 실패
+          alert("인증번호가 일치하지 않습니다. 다시 확인해주세요.");
+        }
+      })
+      .catch((e) => {
+        console.error("Error during auth code check:", e);
+        alert("에러가 발생했습니다.");
+      });
+  }  
