b17da26e72e4b2174f5a26dca1ef4b007fb50b0f
Jian Jang <pureunnabi@gmail.com>
2023-09-13 23:03:40 +0900

    로그아웃 연동

--- app/src/main/assets/js/setting.js
+++ app/src/main/assets/js/setting.js
@@ -1,7 +1,6 @@
 function fetchAndDisplayUserName() {
   // localStorage에서 token 값을 가져와서 출력
 const token = localStorage.getItem('token');
-console.log(token);
 
 axios
 .get('http://43.201.10.121:3000/users/getUserInfo', {
@@ -35,8 +34,23 @@ function logout(){
 function check(flag){
   let confirmDiv = document.getElementsByClassName('logout-confirm')[0];
   confirmDiv.style.visibility = "hidden";
+  const id = localStorage.getItem('userid');
   if(flag){
-      window.location.href = "./login.html";
+    axios
+    .post('http://43.201.10.121:3000/users/logout', {
+      userid: id, 
+    })
+    .then((response) => {
+      if (response.status === 200) {
+        // 로그아웃이 성공적으로 처리된 경우
+        window.location.href = './login.html'; // 로그인 페이지로 이동하거나 다른 조치를 취할 수 있습니다.
+      } else {
+        console.error('로그아웃 실패:', response.data.message); // 실패한 경우 오류 메시지를 출력할 수 있습니다.
+      }
+    })
+    .catch((error) => {
+      console.error('로그아웃 중 오류 발생:', error);
+    });
   }
 }
 
