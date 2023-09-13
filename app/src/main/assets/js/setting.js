3141844371932ab550efc504d6ecebc78206ef9c
seohyunCho <shanny06@naver.com>
2023-09-13 15:57:54 +0900

    login.js 수정 및 로그아웃 confirm 재생성

--- app/src/main/assets/js/setting.js
+++ app/src/main/assets/js/setting.js
@@ -26,4 +26,17 @@ axios
   
 }
 
+
+function logout(){
+  document.getElementsByClassName('logout-confirm')[0].style.visibility = "visible";
+}
+
+function check(flag){
+  let confirmDiv = document.getElementsByClassName('logout-confirm')[0];
+  confirmDiv.style.visibility = "hidden";
+  if(flag){
+      window.location.href = "./login.html";
+  }
+}
+
 window.addEventListener('load', fetchAndDisplayUserName);
