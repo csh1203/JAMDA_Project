eb89be5fccef978601b72a53e4b2f560457406ed
Jian Jang <pureunnabi@gmail.com>
2023-09-16 23:55:03 +0900

    규칙 불러오기(규칙 페이지)

--- app/src/main/assets/js/rule.js
+++ app/src/main/assets/js/rule.js
@@ -13,6 +13,39 @@ let listTexText=['1set'];
 let alertDiv = document.getElementsByClassName('alert')[0];
 let alertTitle = document.getElementsByClassName('alert-title')[0];
 
+function alertCheck(){
+    alertDiv.style.visibility = "hidden";
+}
+
+function fetchRules() {
+  // 사용자의 Token을 로컬 스토리지에서 가져옵니다.
+  const token = localStorage.getItem("token");
+  
+  // 서버로 GET 요청을 보냅니다.
+  axios.get('http://52.78.221.233:3000/users/getRules', {
+      headers: {
+          Authorization: token // 토큰을 헤더에 포함
+      }
+  })
+  .then((response) => {
+      likeDo = response.data.activity;
+      exerciseTitle = response.data.exercise;
+      exerciseRule = response.data.activityNum;
+      exerciseUnit = response.data.unit;
+      baseExerCount = response.data.count;
+
+      console.log(likeDo);
+      console.log(exerciseTitle);
+      console.log(exerciseRule);
+      console.log(exerciseUnit);
+      console.log(baseExerCount);
+  })
+  .catch((error) => {
+      console.error('Error fetching data:', error);
+  });
+}
+
+fetchRules();
 
 
 function scrollBox() {
