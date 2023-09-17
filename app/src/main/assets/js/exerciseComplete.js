3b5422dceda56cce74b1b63203655555e31ddf37
Jian Jang <pureunnabi@gmail.com>
2023-09-18 01:10:04 +0900

    완료된 날짜 찍기

--- app/src/main/assets/js/exerciseComplete.js
+++ app/src/main/assets/js/exerciseComplete.js
@@ -5,14 +5,15 @@ function addDate(){
     const id = localStorage.getItem('userid');
 
     axios
-    .post("http://52.78.221.233:3000/users/increaseCount", {
+    .post("http://52.78.221.233:3000/users/dateCalendar", {
         userid: id,
     })
     .then((response) => {
-        console.log("증가");
+        console.log("추가 완료");
     })
     .catch((e) => {
         console.log(err);
     }); 
   
 }
+addDate();
